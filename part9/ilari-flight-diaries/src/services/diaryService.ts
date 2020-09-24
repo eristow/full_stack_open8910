be9b347a754f09cfc1d57107b2888790cc700d2d
Evan Ristow <evan.ristow1@gmail.com>
2020-09-24 12:24:01 -0400

    Finish 9.c lecture

--- part9/ilari-flight-diaries/src/services/diaryService.ts
+++ part9/ilari-flight-diaries/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaries from '../../data/diaries';
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import { NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry } from '../types';
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
@@ -14,12 +14,24 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addEntry = (): [] => {
-  return [];
+const addEntry = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
 };
 
 export default {
   getEntries,
   addEntry,
   getNonSensitiveEntries,
+  findById,
 };
